f978b7374ee27fd1b8430ed11cae4e4d78ced658
Samaa Medhat <samaa@aserver.com>
2011-02-26 10:20:52 +0000

    Creation of index should only be done when this is a new view

--- model/views/page/view_page_tag_list.py
+++ model/views/page/view_page_tag_list.py
@@ -11,8 +11,8 @@ if not connection.viewExists(rootobject, viewname):
     view.setCol('tag', q.enumerators.OsisType.STRING, False)
     connection.viewAdd(view)
 
-indexes = ['tag', 'guid']
-for field in indexes:
-    connection.runQuery("CREATE INDEX %(field)s_%(schema)s_%(view)s ON %(schema)s.%(view)s (%(field)s)"%{'schema':rootobject, 
-                                                                                                         'view': viewname , 
-                                                                                                         'field':field})
+    indexes = ['tag', 'guid']
+    for field in indexes:
+        connection.runQuery("CREATE INDEX %(field)s_%(schema)s_%(view)s ON %(schema)s.%(view)s (%(field)s)"%{'schema':rootobject, 
+                                                                                                             'view': viewname , 
+                                                                                                             'field':field})

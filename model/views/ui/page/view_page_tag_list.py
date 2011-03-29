04e6daa1a55823c1d9bdee54762ad2dbfe45e35b
bonnek <devnull@localhost>
2011-03-29 16:12:16 +0200

    taskletified views

--- model/views/ui/page/view_page_tag_list.py
+++ model/views/ui/page/view_page_tag_list.py
@@ -1,19 +1,22 @@
-from pylabs.InitBase import q
+__tags__ = 'setup'
+
 from osis.store.OsisDB import OsisDB
 
-domain = 'ui'
-rootobject = 'page'
-viewname = 'view_page_tag_list'
+def main(q, i, p, params, tags):
 
-connection = OsisDB().getConnection('main')
-
-if not connection.viewExists(domain, rootobject, viewname):
-    view = connection.viewCreate(domain, rootobject, viewname)
-    view.setCol('tag', q.enumerators.OsisType.STRING, False)
-    connection.viewAdd(view)
-
-    indexes = ['tag']
-    for field in indexes:
-        connection.runQuery("CREATE INDEX %(field)s_%(schema)s_%(view)s ON %(schema)s.%(view)s (%(field)s)"%{'schema': '%s_%s' % (domain, rootobject), 
-                                                                                                             'view': '%s_%s' % (domain, viewname) , 
-                                                                                                             'field':field})
+    domain = 'ui'
+    rootobject = 'page'
+    viewname = 'view_page_tag_list'
+    
+    connection = OsisDB().getConnection('main')
+    
+    if not connection.viewExists(domain, rootobject, viewname):
+        view = connection.viewCreate(domain, rootobject, viewname)
+        view.setCol('tag', q.enumerators.OsisType.STRING, False)
+        connection.viewAdd(view)
+    
+        indexes = ['tag']
+        for field in indexes:
+            connection.runQuery("CREATE INDEX %(field)s_%(schema)s_%(view)s ON %(schema)s.%(view)s (%(field)s)"%{'schema': '%s_%s' % (domain, rootobject), 
+                                                                                                                 'view': '%s_%s' % (domain, viewname) , 
+                                                                                                                 'field':field})

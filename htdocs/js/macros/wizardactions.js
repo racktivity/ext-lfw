fc0a53f7027bf8b365a1608fd0750834b75a5a0c
tdeconin <devnull@localhost>
2011-04-07 15:48:44 +0200

    Fixing js URL

--- htdocs/js/macros/wizardactions.js
+++ htdocs/js/macros/wizardactions.js
@@ -9,6 +9,6 @@ var render = function(options) {
 	
 	options.addDependency(cb, ['/static/jswizards/js/jswizards_client.js', '/static/jswizards/js/jswizards.js', '/static/jswizards/js/jswizards_oldstyle.js','https://github.com/malsup/form/raw/master/jquery.form.js?v2.43', '/static/js/jquery.floatbox.1.0.8.js','/static/js/jquery-datepicker/demo/scripts/jquery.datePicker.js', '/static/js/jquery-datepicker/demo/scripts/jquery.datePicker.js','/static/js/jquery-datepicker/demo/scripts/date.js', '/static/js/jquery.ui.datetimepicker.js','/static/js/jQueryBubblePopup.v2.3.1_2/jquery.bubblepopup.v2.3.1.min.js','/static/js/jquery-tooltip/js/jtip.js'])	
 	options.addCss({'id': 'wizardactions', 'tag': 'link', 'params': {'rel': 'stylesheet', 'href': 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css'}})
-	options.addCss({'id': 'wizardaction', 'tag': 'link', 'params': {'rel': 'stylesheet', 'href': '/static/lfw/js/libs/jQueryBubblePopup.v2.3.1_2/jquery.bubblepopup.v2.3.1.css'}})
+	options.addCss({'id': 'wizardaction', 'tag': 'link', 'params': {'rel': 'stylesheet', 'href': '/static/js/libs/jQueryBubblePopup.v2.3.1_2/jquery.bubblepopup.v2.3.1.css'}})
 };
 register(render);

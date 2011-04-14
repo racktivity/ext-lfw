849470a128381e4e73d0d43f1f282902470d1a5a
Mohammad Nour <mojo@incubaid.com>
2011-04-14 19:00:22 +0200

    Review and minor changes.

--- htdocs/js/macros/jqplot.js
+++ htdocs/js/macros/jqplot.js
@@ -1,33 +1,29 @@
 var render = function(options) {
     var TEMPLATE_NAME = 'plugin.jqplot';
     var $this = $(this);
-    
     var data_dict = $.parseJSON(options.body);
     var height = data_dict.height || 700;
-    
     var width = data_dict.width || 700;
-    
     var chart_data = data_dict.chart_data;
-    
-    var chart_title = data_dict.title || "Exponential Line2";
-    
+    var chart_title = data_dict.title || "jqPlot Chart/Plot";
     var renderer = data_dict.renderer;
-    var renderer_path;
+    var renderer_dependency;
+
     if (renderer){
         var renderer_name = renderer.replace("$.jqplot.", "");
         renderer_name = renderer_name.charAt(0).toLowerCase().concat(renderer_name.slice(1));
-        renderer_path = "js/libs/jqplot/plugins/jqplot.renderer_name.js".replace("renderer_name", renderer_name)
+        renderer_dependency = "js/libs/jqplot/plugins/jqplot.renderer_name.js".replace("renderer_name", renderer_name)
     }
-    
-    console.log("redered_path : " +renderer_path);
-    
+
+    console.log("redered_path : " + renderer_dependency);
+
     var chart_div = data_dict.chart_div
     
     var cb = function(){
         
         var series;
         if (renderer){
-             var series = {series:[{renderer:eval(renderer)}]};
+             series = {series:[{renderer:eval(renderer)}]};
         }
         $.jqplot.config.enablePlugins = true;
         plot1 = $.jqplot(chart_div, chart_data, series);
@@ -49,10 +45,11 @@ var render = function(options) {
      'js/libs/jqplot/src/jqplot.sprintf.js',
      'js/libs/jqplot/src/jsdate.js',
      "js/libs/jqplot/src/jqplot.themeEngine.js"];
-    if (renderer_path){
-        dependencies.push(renderer_path);
+
+    if (renderer_dependency){
+        dependencies.push(renderer_dependency);
     }
-    
+
     options.addDependency(cb, dependencies); 
 
     $.template(TEMPLATE_NAME, '<div><div id=${chart_div} style="height:${height}px;width:${width}px; "></div></div>');

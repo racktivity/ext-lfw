4bd3475e8af2e876a43d5260b7f6cbd3a1ef9ae4
Jo De Boeck <jdeboeck@aserver.com>
2011-04-26 14:18:09 +0200

    Parameterize youtube

--- htdocs/js/macros/youtube.js
+++ htdocs/js/macros/youtube.js
@@ -2,9 +2,9 @@ var render = function(options) {
   $this = $(this);
   
   var config = {
-    movieid: "grbSQ6O6kbs",
-    width: "270",
-    height: "227"
+    movieid: options.params.movieid || "grbSQ6O6kbs",
+    width: options.params.width || "270",
+    height: options.params.height || "227"
   };    
   
   $.template('plugin.youtube.content', '<div><iframe title="YouTube video player" width="${width}" height="${height}" src="http://www.youtube.com/embed/${movieid}" frameborder="0" allowfullscreen></iframe></div>');

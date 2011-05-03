2ac713b93cecc5535f075a886294e46ce86818f2
Shehab El Noury <selnoury@aserver.com>
2011-05-03 18:41:05 +0300

    Macro now takes all 6 headers. Headers are indented.

--- htdocs/js/macros/toc.js
+++ htdocs/js/macros/toc.js
@@ -5,16 +5,37 @@ var render = function(options) {
     var tocTemplate = $.tmpl('plugin.page.toc', {});
     $this.append(tocTemplate);
     
-    //var body = $(options.body);
-    var value = options.params.tags || "h1, h2, h3";
+    var value = "H1, H2, H3, H4, H5, H6";
+    var toc_ul = $('<ul>');
 
-    $("#toc").append('<p>Table of Contents:</p>');
+    $("#toc").append('<p><b>Table of Contents:</b></p>');
     options.pagecontent.find(value).each(function(i) {
         var current = $(this);
-        current.attr("id", "title" + i);
-        $("#toc").append("<a id='link" + i + "' href='" + document.URL + "/#title" +
-            i + "'>" + current.html() + "</a><br />");
+        var headerLink = "header" + i;
+        current.attr("id", headerLink);
+
+        if (this.tagName == 'H1') {
+            toc_ul.append("<li><a id='link" + i + "' href='" + document.URL + "/#" + headerLink + "'>" + current.html() + "</a></li>");
+            toc_ul.append('&nbsp;');
+        }
+        else if (this.tagName == 'H2') {
+            toc_ul.append("<ul><li><a id='link" + i + "' href='" + document.URL + "/#" + headerLink + "'>" + current.html() + "</a></li></ul>");
+        }
+        else if (this.tagName == 'H3') {
+            toc_ul.append("<ul><ul><li><a id='link" + i + "' href='" + document.URL + "/#" + headerLink + "'>" + current.html() + "</a></li></ul></ul>");
+        }
+        else if (this.tagName == 'H4') {
+            toc_ul.append("<ul><ul><ul><li><a id='link" + i + "' href='" + document.URL + "/#" + headerLink + "'>" + current.html() + "</a></li></ul></ul></ul>");
+        }
+        else if (this.tagName == 'H5') {
+            toc_ul.append("<ul><ul><ul><ul><li><a id='link" + i + "' href='" + document.URL + "/#" + headerLink + "'>" + current.html() + "</a></li></ul></ul></ul></ul>");
+        }
+        else if (this.tagName == 'H6') {
+            toc_ul.append("<ul><ul><ul><ul><ul><li><a id='link" + i + "' href='" + document.URL + "/#" + headerLink + "'>" + current.html() + "</a></li></ul></ul></ul></ul></ul>");
+        }
+
     });
+    $('#toc').append(toc_ul);
 }
 
 register(render);

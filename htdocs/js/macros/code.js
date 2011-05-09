986c97c1ef2be31350a3f5e49f469c2cf67e0dbc
Ola Mohammad <ola@aserver.com>
2011-05-09 16:30:52 +0200

    PYLABS-56:- some small bugs / improvements for alkira:
    Make the code block in Macros_HOWTO not to go over width of screen by adding a css property to wrap test.

--- htdocs/js/macros/code.js
+++ htdocs/js/macros/code.js
@@ -2,15 +2,16 @@ var render = function(options) {
     var $this = $(this);
     
     var cb = function () {
-	    $.template('plugin.code.highlight', '<div><pre><code>${options}</code></pre></div>');
-	    var c = $.tmpl('plugin.code.highlight', {'options': options.body});
-	  
-	    $this.append(c);
-	
-	    hljs.tabReplace = '    '; // 4 spaces
-	    //hljs.tabReplace = '<span class="indent">\t</span>';
-	    $this.find('pre code').each(function(i, e) {hljs.highlightBlock(e, '    ')});
-	};
+        $.template('plugin.code.highlight', '<div><pre><code>${options}</code></pre></div>');
+        var c = $.tmpl('plugin.code.highlight', {'options': options.body});
+
+        $this.append(c);
+
+        hljs.tabReplace = '    '; // 4 spaces
+        //hljs.tabReplace = '<span class="indent">\t</span>';
+        $this.find('pre code').each(function(i, e) {hljs.highlightBlock(e, '    ')});
+        $this.find('pre code').css("white-space", "pre-wrap");
+    };
     
     options.addCss({'id': 'codemacro', 'tag': 'link', 'params': {'rel': 'stylesheet', 'href': 'http://yandex.st/highlightjs/5.16/styles/default.min.css'}})
     options.addDependency(cb, ['http://yandex.st/highlightjs/5.16/highlight.min.js'])

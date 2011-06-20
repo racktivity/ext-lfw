ee8773e90399c531083324fac579bc5dea59f7e1
mina_magdy <devnull@localhost>
2011-06-20 01:43:11 -0700

    Implemented macro for validating links/marcos in other pages

--- client/AlkiraClient.py
+++ client/AlkiraClient.py
@@ -67,6 +67,22 @@ class Client:
         query = self.connection.page.query(page_info)
         return query
 
+    def listChildPages(self, space, name):
+        """
+        Lists child pages of page "name"
+
+        @type space: String
+        @param space: The name of the space.
+        
+        @type name: String
+        @param name: The name of the parent page.
+        """
+        #Get page guid
+        guid = self._getPageInfo(space, name)[0]["guid"]
+        page_info = 'SELECT "name" FROM ui_page.ui_view_page_list WHERE parent = \'%s\'' %guid
+        query = self.connection.page.query(page_info)
+        return list(name["name"] for name in query)
+
     def pageExists(self, space, name):
         """
         Checks whether a page exists or not.
---
+++ pylabsmacro/pagevalidator/validate.py
@@ -0,0 +1,102 @@
+__author__ = "incubaid"
+#!/usr/bin/env python
+from pylabs.InitBaseCore import q
+import re
+linkRe = re.compile("/[a-zA-Z0-9]*/#/[a-zA-Z0-9/]*")
+macroRe = re.compile("\[\[([a-zA-Z0-9 =]+)\]\]")
+
+def macroExists(macro, space):
+    if q.system.fs.isFile("/opt/qbase5/www/lfw/js/macros/%s.js"%macro):
+        return True
+    if q.system.fs.isDir("/opt/qbase5/lib/python/site-packages/alkira/tasklets/pylabsmacro/%s/"%macro):
+        return True
+    return q.system.fs.isFile("/opt/qbase5/pyapps/%s/impl/portal/jsmacros/%s.js"%(space, macro))
+
+def linkExists(link, client):
+    linkparts = link.split("/", 4)[1:]
+    if len(linkparts) == 4:
+        appname, hashmark, space, page = linkparts
+    elif len(linkparts) == 3:
+        appname, hashmark, space = linkparts
+        page = "Home"
+    else:
+        raise Exception("Page %s is invalid "%link)
+    return client.pageExists(space, page)
+
+def getLinks(body):
+    return linkRe.findall(body)
+
+def getMacros(body):
+    return macroRe.findall(body)
+
+def getPageReport(client, space, name, recursive = False):    
+    result = list()
+    page = client.getPage(space, name)
+    #Check links
+    links = getLinks(page.content)
+    for link in links:
+        ok = linkExists(link, client)
+        result.append((space + "/" + page.name, "link", link, ok))
+
+    #Check Macros
+    macros = getMacros(page.content)
+    for macro in macros:
+        ok = macroExists(macro, space)
+        result.append((space + "/" + page.name, "macro", macro, ok))
+
+    if recursive:
+        for childpage in client.listChildPages(space, name):
+            result += getPageReport(client, space, childpage, True)
+    return result
+
+def getInvalidLinks(appname, spaces = None, pages = None):
+    result = list()
+    client = q.clients.alkira.getClient("localhost", appname)
+    if not spaces:
+        spaces = client.listSpaces()
+    
+    for space in spaces:
+        for page in client.listPages(space):
+            if pages and (page not in pages):
+                continue
+            result += getPageReport(client, space, page, False)
+    return result
+
+def main(q, i, p, params, tags):
+    #import rpdb2
+    #rpdb2.start_embedded_debugger("mypass", fAllowRemote=True)
+    tags = params["tags"].tags
+    appname = p.api.appname
+    client = q.clients.alkira.getClient("localhost", appname)
+    #
+    pages = spaces = None
+    if "pages" in tags:
+        pages = list(item.strip() for item in tags["pages"].split(","))
+    if "spaces" in tags:
+        if tags["spaces"] == "*":
+            spaces = client.listSpaces()
+        else:
+            spaces = list(item.strip() for item in tags["spaces"].split(","))
+    #
+    if pages or spaces:
+        result = getInvalidLinks(appname, spaces, pages)
+    else:
+        result = getPageReport(client, tags["space"], tags["page"], True)
+    html = "<TABLE><TR><TD>Page</TD><TD>URL</TD><TD>Status</TD></TR>\n"
+    row = "<TR><TD>%s</TD><TD>%s</TD><TD>%s</TD><TR>\n"
+    for item in result:
+        parenturl = "/%s/#/%s"%(appname, item[0])
+        if item[1] == "link":
+            html += row%(
+                "[%s](%s)"%(parenturl, parenturl),
+               "[%s](%s)"%(item[2], item[2]),
+                "OK" if item[3] else "INVALID"
+            )
+        elif item[1] == "macro":
+            html += row%(
+                "[%s](%s)"%(parenturl, parenturl),
+                "[[%s]]"%(item[2]),
+                "OK" if item[3] else "INVALID"
+            )
+    html += "</TABLE>"
+    params['result'] = "%s"%html

edf9ef0f3729e2da419d0cf61899c36288b73f55
paulus_z <paulus@racktivity.com>
2011-06-22 07:43:08 -0700

    Added code to allow for scales (and the grid lines). Currently it's broken :-( i.e. the graph renders but the scales not (or onclomplete But checked in so it can be worked on.

--- htdocs/js/macros/protovis.js
+++ htdocs/js/macros/protovis.js
@@ -2,8 +2,8 @@ var render = function(options) {
     var TEMPLATE_NAME = 'plugin.protovis'; // this is only local I think
     var $this = $(this);
     var data_dict = $.parseJSON(options.body);
-    var height = data_dict.height || 700;
-    var width = data_dict.width || 700;
+    var height = data_dict.height || 400;
+    var width = data_dict.width || 400;
     var protovis_id = data_dict.protovis_id;
     var data = data_dict.data; // like this: [1, 1.2, 1.7, 1.5, .7, .5, .2]
     var horz_unit = data_dict.horz_unit; // should we name this like this?
@@ -12,18 +12,55 @@ var render = function(options) {
     
     
     var cb = function(){
+        var x = null;
+        var y = null;
+        
         $.template(TEMPLATE_NAME, '<div id=${protovis_id} style="height:${height}px;width:${width}px; "></div>');
         $.tmpl(TEMPLATE_NAME, {protovis_id:protovis_id, height:height, width:width}).appendTo($this);
         
-        new pv.Panel()
+        // make scales if provided
+        if (data_dict.x_scale) {
+            x = pv.Scale.linear(0, data_dict.x_scale).range(0, width);
+        }
+        
+        if (data_dict.y_scale) {
+            y = pv.Scale.linear(0, data_dict.y_scale).range(0, height);
+        }
+        
+        var vis = new pv.Panel()
             .canvas(protovis_id)
             .width(width)
             .height(height)
-          .add(pv.Line)
+        
+        vis.add(pv.Line)
             .data(data)
             .bottom(function(d) {return d * vert_unit;} )
-            .left(function() {return this.index * horz_unit + horz_offset;})
-          .root.render();
+            .left(function() {return this.index * horz_unit + horz_offset;});
+        
+        // render scales if provided
+        // !!!! note the grid shows on Chrome but not on Firefox
+        // the scales labels don't show on either
+        if (x) {
+            vis.add(pv.Rule)
+                .data(x.ticks())
+                .strokeStyle("#eee")
+                .left(x)
+              .anchor("bottom").add(pv.Label)
+                .text(x.tickFormat);
+        }
+        
+        if (y) {
+            vis.add(pv.Rule)
+                .data(y.ticks())
+                .strokeStyle("#eee")
+                .bottom(y)
+              .anchor("left").add(pv.Label)
+                .text(y.tickFormat);
+        }
+        
+        
+        vis.root.render();
+        
     };
     
     var dependencies = ["/static/lfw/js/libs/protovis-3.2/protovis-d3.2.js"];

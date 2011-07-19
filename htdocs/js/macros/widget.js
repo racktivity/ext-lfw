e8a6b6ff09a520b0957f7c4837bde9a5dcdfb5b7
kneirinc <devnull@localhost>
2011-07-19 13:59:48 +0200

    make it possible to not be able to collapse the widget in the widget macro

--- htdocs/js/macros/widget.js
+++ htdocs/js/macros/widget.js
@@ -1,43 +1,35 @@
 
 var render = function(options) {
     var $this = $(this);
-    var params = $.extend({title: '',
-                           width: '',
-                           height: ''}, options.params);
+    var params = $.extend({title: '', width: '', height: '', toggle: true}, options.params);
 
     var body = $("<div>", {'class': 'widget-body'});
 
 
     var titlebar = $("<div>", {'class': 'widget-titlebar'})
-                        .append($("<div>", {'class': 'widget-title'}).text(params.title))
-                        .append($("<div>", {'class': 'widget-collapse ui-icon ui-icon-circle-minus'})
-                                    .click(function(){
-                                        var btn = $(this);
-                                        btn.parents(".widget-body")
-                                            .find(".widget-content")
-                                            .slideToggle("fast", function(){
-                                                if ($(this).css("display") === "none"){
-                                                    btn.removeClass("ui-icon-circle-minus")
-                                                        .addClass("ui-icon-circle-plus");
-                                                } else {
-                                                    btn.removeClass("ui-icon-circle-plus")
-                                                        .addClass("ui-icon-circle-minus");
-                                                }
-                                            });
-                                    }));
+        .append($("<div>", {'class': 'widget-title'}).text(params.title));
+    if (params.toggle && params.toggle !== "false") {
+        titlebar.append($("<div>", {'class': 'widget-collapse ui-icon ui-icon-circle-minus'})
+            .click(function() {
+                var btn = $(this);
+                btn.parents(".widget-body").find(".widget-content").slideToggle("fast", function(){
+                    if ($(this).css("display") === "none"){
+                        btn.removeClass("ui-icon-circle-minus").addClass("ui-icon-circle-plus");
+                    } else {
+                        btn.removeClass("ui-icon-circle-plus").addClass("ui-icon-circle-minus");
+                    }
+                });
+            }));
+    }
 
     var content = $("<div>", {'class': 'widget-content'})
-                        .html(options.renderWiki(options.body))
-                        .css('width', params.width)
-                        .css('height', params.height);
+        .html(options.renderWiki(options.body))
+        .css('width', params.width)
+        .css('height', params.height);
 
-    body.append(titlebar)
-        .append(content);
+    body.append(titlebar).append(content);
 
-    options.addCss({'id': 'widget',
-                    'tag': 'link',
-                     'params': {'rel': 'stylesheet',
-                                'href': 'css/macros/widget.css'}});
+    options.addCss({'id': 'widget', 'tag': 'link', 'params': {'rel': 'stylesheet', 'href': 'css/macros/widget.css'}});
     $this.empty();
     options.swap(body, undefined, true);
 };

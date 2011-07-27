ff280ebc472fc5a8566be612175591d127e0aea8
mazmy <devnull@localhost>
2011-07-27 15:02:39 +0200

    implement small js plugin for events locking

---
+++ htdocs/js/lock.js
@@ -0,0 +1,33 @@
+
+$(function($){
+    $.fn.lock = function(name, handler){
+        return this.each(function(){
+            var $this = $(this);
+            var ldata = $this.data("_lock");
+            if (!ldata){
+                ldata = {};
+                $this.data("_lock", ldata);
+            }
+            
+            if (!(name in ldata)){
+                ldata[name] = {h: [], f: false};
+            }
+            
+            var l = ldata[name];
+            
+            if (handler !== undefined && $.isFunction(handler)){
+                l.h.push(handler);
+            } else {
+                l.f = true;
+            }
+            
+            if (l.f) {
+                $.each(l.h, function(i, v){
+                    v();
+                });
+            }
+            
+            $this.data("_lock", ldata);
+        });
+    };
+})(jQuery);

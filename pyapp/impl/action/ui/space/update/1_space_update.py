24ca9bc47701f8e73e5492717b85f6da60565761
kneirinc <devnull@localhost>
2011-08-02 22:06:52 +0200

    fixed a bug in the space update tasklet

--- pyapp/impl/action/ui/space/update/1_space_update.py
+++ pyapp/impl/action/ui/space/update/1_space_update.py
@@ -2,7 +2,7 @@ from alkira import Alkira
 
 def main(q, i, p, params, tags):
     alkira = Alkira(p.api)
-    tags = param.get("tags")
+    tags = params.get("tags")
     alkira.updateSpace(params['spaceguid'], params['name'], tags.split(" ") if tags else None,
         order=params['order'] if params['order'] else 10000)
     params['result'] = True

3e58f8df1f7252af323694389577d653e42f80fc
kneirinc <devnull@localhost>
2012-01-03 15:48:29 +0100

    added the ability to enable/disable shadow on jqplot

--- htdocs/js/macros/jqplot.js
+++ htdocs/js/macros/jqplot.js
@@ -14,17 +14,18 @@ var render = function(options) {
     var showTicks = data_dict.showTicks;
     var lineWidth = data_dict.lineWidth;
     var axesMin = data_dict.axesMin;
-	var rendererOptions = $.extend({}, data_dict.rendererOptions);
+    var rendererOptions = $.extend({}, data_dict.rendererOptions);
     var xticks = data_dict.xticks || [];
     var yticks = data_dict.yticks || [];
     var xtickformat = data_dict.xtickformat || '';
     var ytickformat = data_dict.ytickformat || '';
+    var shadow = data_dict.shadow;
     var renderer_dependency;
 
-    if (renderer){
+    if (renderer) {
         var renderer_name = renderer.replace("$.jqplot.", "");
         renderer_name = renderer_name.charAt(0).toLowerCase().concat(renderer_name.slice(1));
-        renderer_dependency = "/static/lfw/js/libs/jqplot/src/plugins/jqplot.renderer_name.js".replace("renderer_name", renderer_name)
+        renderer_dependency = "/static/lfw/js/libs/jqplot/src/plugins/jqplot." + renderer_name + ".js";
     }
 
     console.log("redered_path : " + renderer_dependency);
@@ -36,20 +37,31 @@ var render = function(options) {
     var chart_div = getID();
 
     var cb = function(){
-        var opts = {axesDefaults:{min: axesMin},
-                    axes:{xaxis:{ticks: xticks,
-                                 tickOptions:{formatString: xtickformat}},
-                          yaxis:{ticks: yticks,
-                                 tickOptions:{formatString: ytickformat}}},
-                    seriesDefaults: {lineWidth: lineWidth,
-                                    markerOptions: {show: showTicks}}};
+        var opts = {
+            axesDefaults: { min: axesMin },
+            axes: {
+                xaxis: {
+                    ticks: xticks,
+                    tickOptions: { formatString: xtickformat }
+                },
+                yaxis: {
+                    ticks: yticks,
+                    tickOptions: { formatString: ytickformat }
+                }
+            },
+            seriesDefaults: {
+                lineWidth: lineWidth,
+                markerOptions: { show: showTicks }
+            },
+            grid: {
+                shadow: shadow
+            }
+        };
         if (renderer){
-             $.extend(opts, { series:[{renderer:eval(renderer),
-								 rendererOptions: rendererOptions}],
-			            });
+             $.extend(opts, { series: [ { renderer: eval(renderer), rendererOptions: rendererOptions } ] });
         }
         $.jqplot.config.enablePlugins = true;
-        plot1 = $.jqplot(chart_div, chart_data, opts);
+        $.jqplot(chart_div, chart_data, opts);
     };
 
     var dependencies = ["/static/lfw/js/libs/jqplot/src/jquery.jqplot.js",
@@ -75,13 +87,15 @@ var render = function(options) {
 
     width =  isNaN(Number(width)) ? width: width + "px";
     height = isNaN(Number(height)) ? height: height + "px";
-    
-    $.template(TEMPLATE_NAME, '<div><div id="${chart_div}" style="height:${height};width:${width}; "></div></div>');
-    $.tmpl(TEMPLATE_NAME, {chart_div:chart_div, height:height, width:width}).appendTo($this);
 
-    options.addCss({'id': 'jqplot', 'tag': 'link', 'params': {'rel': 'stylesheet', 'href': 'js/libs/jqplot/src/jquery.jqplot.css'}});
+    $.template(TEMPLATE_NAME, '<div><div id="${chart_div}" style="height:${height};width:${width}; "></div></div>');
+    $.tmpl(TEMPLATE_NAME, { chart_div: chart_div, height: height, width: width }).appendTo($this);
+
+    options.addCss({ 'id': 'jqplot', 'tag': 'link',
+        'params': { 'rel': 'stylesheet', 'href': 'js/libs/jqplot/src/jquery.jqplot.css' } });
 
     options.addDependency(cb, dependencies, true);
-}
+};
+
 register(render);
 

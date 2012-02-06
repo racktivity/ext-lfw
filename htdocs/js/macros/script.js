08d01052b3354278af32c0bd9523a0428dedb600
Mohammed Azmy <mohammed.azmy@aserver.com>
2012-02-06 09:37:02 +0200

    make sure src files are loaded before running the body

--- htdocs/js/macros/script.js
+++ htdocs/js/macros/script.js
@@ -5,15 +5,19 @@ var render = function(options) {
     var $this = $(this);
     var body = options.body;
     var src = options.params.src;
-    var func = function() {};
+    
+    var func = function() {
+        if (body){
+            var script = $("<script>").text(body);
+            $("head").append(script);
+        }
+    };
+    
     if (src){
         dominoes(src, func);
+    } else {
+        func();
     }
-    if (body){
-        var script = $("<script>").text(body);
-        $("head").append(script);
-    }
-
 }
 
 register(render);

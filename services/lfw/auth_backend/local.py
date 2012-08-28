8624acb0173c32491ab173ddfc591ab427768791
kneirinc <devnull@localhost>
2012-08-28 16:54:14 +0200

    re-add calling the auth service directly from alkira and reread the config file each time for the local auth backend DEVDCPM-1145 / DEVDCPM-1027

--- services/lfw/auth_backend/local.py
+++ services/lfw/auth_backend/local.py
@@ -1,12 +1,25 @@
 import hashlib, authbackend
 from pylabs import q, p
 
+def reload_config(func):
+
+    def wrapper(*args, **kwargs):
+        args[0]._reloadConfig() #pylint: disable=W0212
+        return func(*args, **kwargs)
+
+    return wrapper
+
 class LocalAuthBackend(authbackend.AuthBackend):
     def __init__(self):
         super(LocalAuthBackend, self).__init__()
+        self.config = None
+        self._reloadConfig()
+
+    def _reloadConfig(self):
         self.config = q.tools.inifile.open(q.system.fs.joinPaths(q.dirs.pyAppsDir, p.api.appname, "cfg", \
             "auth_local.cfg"))
 
+    @reload_config
     def verifyUserIdentity(self, login, password):
         if self.config.checkSection(login) and self.config.checkParam(login, "password"):
             md5 = hashlib.md5()
@@ -16,6 +29,7 @@ class LocalAuthBackend(authbackend.AuthBackend):
 
         return False
 
+    @reload_config
     def createUser(self, userinfo):
         if self.config.checkSection(userinfo["login"]):
             q.errorconditionhandler.raiseError("User %s already exists in the backend." % userinfo["login"])
@@ -29,6 +43,7 @@ class LocalAuthBackend(authbackend.AuthBackend):
 
         return super(LocalAuthBackend, self).createUser(userinfo)
 
+    @reload_config
     def deleteUser(self, userid):
         login = super(LocalAuthBackend, self).deleteUser(userid)
         if not self.config.checkSection(login):
@@ -38,6 +53,7 @@ class LocalAuthBackend(authbackend.AuthBackend):
         self.config.write()
         return True
 
+    @reload_config
     def updateUser(self, userid, userinfo):
         login = super(LocalAuthBackend, self).updateUser(userid, userinfo)
         if not self.config.checkSection(login):

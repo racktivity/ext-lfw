c8d7eeb228d7eed7cef363119d5476a76f5a4184
slockij <devnull@localhost>
2012-11-06 11:40:01 +0100

    DEVDCPM-426 - changes in notification macro. apply authorization and prevent from running forever Reviewed by: Rick

--- htdocs/js/macros/notification.js
+++ htdocs/js/macros/notification.js
@@ -1,9 +1,11 @@
 var render = function(options) {
 
     var $this = $(this);
+    var elemId = 'notif-' + (new Date()).getTime() + parseInt(Math.random()*100000);
     var defaultmsg = $("<div>").html("The curent page is out of date click <a href=\"javascript:location.reload();\">here</a> to reload.");
     var params = $.extend({content: defaultmsg, delay:-1, cssclass: "customNotify"}, options.params);
     params.delay = Number(params.delay);
+    $this.attr('id', elemId);
     $this.addClass(params.cssclass);
     $this.html(params.content);
 
@@ -13,8 +15,12 @@ var render = function(options) {
     tagstring += ' page:' + options.page;
     tagstring += ' space:' + options.space;
     var timetagstring = tagstring + ' updatetime:' + updatetime;
-    
+
     function checkupdate(){
+        // check if page has not changed
+        if ($('#' + elemId).length === 0) {
+            return;
+        }
         $.ajax({
             url:   'appserver/rest/ui/portal/generic',
             data: {
@@ -22,18 +28,17 @@ var render = function(options) {
                 macroname: "notification"
               },
             dataType: 'json',
-            global: false,
             success: function (data, textStatus, jqXHR) {
-                        checkupdate();
-                        if (data > updatetime){
-                            updatetime = data;
-                            timetagstring = tagstring + ' updatetime:' + updatetime;
-                            $this.jmNotify({delay:params.delay});
-                        }
-                      },
+                checkupdate();
+                if (data > updatetime){
+                    updatetime = data;
+                    timetagstring = tagstring + ' updatetime:' + updatetime;
+                    $this.jmNotify({delay:params.delay});
+                }
+            },
             error:   function (data, textStatus, jqXHR) {
-                        checkupdate();
-                     }
+                window.setTimeout(checkupdate, 2000); // wait 2 seconds and try again
+            }
         });
 
     };

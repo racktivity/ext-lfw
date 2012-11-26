bd7928a87986439ea554cc12263b1f08fdb437a3
kneirinc <devnull@localhost>
2012-11-26 15:29:12 +0100

    fix groupguids argument of the authoriserule.find function Reviewed by: rvanover

--- pyapp/impl/action/ui/authoriserule/find/1_authoriserule_find.py
+++ pyapp/impl/action/ui/authoriserule/find/1_authoriserule_find.py
@@ -3,7 +3,7 @@ def main(q, i, p, params, tags):
     filterObject = p.api.model.ui.authoriserule.getFilterObject()
     exact_properties = params['exact_properties'] or ()
 
-    properties = ('groupguid', 'function', 'context')
+    properties = ('groupguids', 'function', 'context')
     for property_name, value in params.iteritems():
         if property_name in properties and not value in (None, ''):
             exact = property_name in exact_properties
